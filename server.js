214eaaf82ed9ef690b367d3786068e7799cb6524
rajithrm <rajithrm.ibirix@gmail.com>
2026-03-25 20:14:03 +0530

    Added signup API with user model and MongoDB integration

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const User = require("./models/userModel")
 
 app.use (express.json());
 
@@ -11,6 +12,33 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req,res)=>{
     res.send("Api is running....");
+});
+
+app.post("/api/signup", async (req,res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        
+        const userExists = await User.findOne ({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
